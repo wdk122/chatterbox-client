be45253e4620509c1855450e655059018ce7dcff
Bill Kilmer <wdk122@gmail.com>
2015-04-06 14:23:08 -0700

    Introductory stuff.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,9 @@
 // YOUR CODE HERE:
+// https://api.parse.com/1/classes/chatterbox
 
+
+var message = {
+  'username': 'billkilmer',
+  'text': 'it is going to rain tomorrow',
+  'roomname': 'paloAlto'
+};
